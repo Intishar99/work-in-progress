bbe8e9bbde4dfc30790e1e682fb0856007a4c718
Shabab Intishar Rahman <45306394+Intishar99@users.noreply.github.com>
2023-07-14 09:16:04 +0600

    Update streamlit.trial.py

--- streamlit.trial.py
+++ streamlit.trial.py
@@ -9,19 +9,17 @@ def process_file(uploaded_file):
     data = pd.read_csv(uploaded_file)
 
     # Specify your topic columns
-    topic_columns =  ['Motion - Homework', 'Units - Recap Quiz',
-       'Units and Prefixes - Grade 9 - Quiz', 'Measurements Homework ',
-       'Displacement - Construction ', 'Measurements - Cambridge Grade 9',
-       'Motion Equations and Graphs', 'Hookes Law - Graph',
-       'Motion Equations and Graphs - Exam', 'Moments - Worksheet',
-       'Forces and Moments - Cambridge', 'Thermal Physics Assignment',
-       'Sound and Electromagnetism', 'Static Electricity - Grade 10 Cambridge',
-       'Electrical Quantities Exam', 'Mock 1 - P4', 
-     'Mock 2 - P4' ]
+    topic_columns = ['Motion - Homework', 'Units - Recap Quiz',
+                     'Units and Prefixes - Grade 9 - Quiz', 'Measurements Homework ',
+                     'Displacement - Construction ', 'Measurements - Cambridge Grade 9',
+                     'Motion Equations and Graphs', 'Hookes Law - Graph',
+                     'Motion Equations and Graphs - Exam', 'Moments - Worksheet',
+                     'Forces and Moments - Cambridge', 'Thermal Physics Assignment',
+                     'Sound and Electromagnetism', 'Static Electricity - Grade 10 Cambridge',
+                     'Electrical Quantities Exam', 'Mock 1 - P4', 'Mock 2 - P4']
 
     # Your existing code for generating frequent itemsets
-    data['Id '] = data['Id '].astype(str)
-    data.rename(columns={'Id ': 'Id'}, inplace=True)
+    data['Id'] = data['Id'].astype(str)
     transactional_data = []
     for index, row in data.iterrows():
         student_id = row['Id']
@@ -30,19 +28,17 @@ def process_file(uploaded_file):
             mark = row[topic_column]
             if mark < 60:
                 transactional_data.append((student_id, topic))
-    
+
     transactional_df = pd.DataFrame(transactional_data, columns=['student_id', 'topic'])
     transactions = transactional_df.groupby('student_id')['topic'].apply(list).tolist()
     te = TransactionEncoder()
-    te_ary = te.fit(transactions).transform(transactions)
+    te_ary = te.fit_transform(transactions)
     df = pd.DataFrame(te_ary, columns=te.columns_)
     frequent_itemsets = fpgrowth(df, min_support=0.5, use_colnames=True)
-    frequent_itemsets['itemsets'] = frequent_itemsets['itemsets'].astype(str)
-    frequent_itemsets_filtered = frequent_itemsets[frequent_itemsets['itemsets'].apply(lambda x: len(x) >= 2)]
-
+    frequent_itemsets['itemsets'] = frequent_itemsets['itemsets'].apply(lambda x: ", ".join(x))
+    frequent_itemsets_filtered = frequent_itemsets[frequent_itemsets['itemsets'].apply(lambda x: len(x.split(", ")) >= 2)]
 
     return frequent_itemsets_filtered
-    #return frequent_itemsets
 
 # Create Streamlit app
 def main():
