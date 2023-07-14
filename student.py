884f2d83e74dc3fdfc8454439b45f404689beecb
Shabab Intishar Rahman <45306394+Intishar99@users.noreply.github.com>
2023-07-14 09:44:52 +0600

    Update student.py

--- student.py
+++ student.py
@@ -2,25 +2,24 @@ import streamlit as st
 
 def main():
     st.title("Student Dashboard")
-
-    # Student information
-    st.subheader("Student Details")
-    student_name = st.text_input("Name")
+    
+    # Student's name
+    student_name = st.text_input("Enter student's name")
+    
+    # Student picture
     student_picture = st.image("placeholder_image.jpg", caption="Student Picture")
-
-    # Subject marks
-    st.subheader("Subject Marks")
-    subjects = ["Math", "Science", "English"]
+    
+    # List of subjects and marks
+    subjects = ['Math', 'Science', 'English']
     marks = []
     for subject in subjects:
-        mark = st.number_input(subject, min_value=0, max_value=100, value=0)
+        mark = st.number_input(f"Enter mark for {subject}", min_value=0, max_value=100, value=0)
         marks.append(mark)
-
+    
     # Revise button
-    st.subheader("Revise")
     if st.button("Revise"):
-        st.success("Click the link below to watch a revision video:")
-        st.markdown("[Revision Video](https://www.example.com/revision)")
-
+        st.write("Click on the link below to watch the revision video.")
+        st.markdown("[Revision Video](https://www.example.com)")
+        
 if __name__ == "__main__":
     main()
