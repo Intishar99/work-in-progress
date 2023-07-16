4a46876132896a7bf3753a8f2e99205a9d9fee5d
Shabab Intishar Rahman <45306394+Intishar99@users.noreply.github.com>
2023-07-16 15:27:38 +0600

    Update streamlit.trial.py

--- streamlit.trial.py
+++ streamlit.trial.py
@@ -57,8 +57,8 @@ def main():
         
         # Display images
         st.subheader("Images")
-        st.image("image1.png", caption="Cluster with marks above 60")
-        st.image("image2.png", caption="Cluster with marks blow 60")
+        st.image("placeholder_image.jpg", caption="Student Picture")
+       
        
 # Run the Streamlit app
 if __name__ == "__main__":
